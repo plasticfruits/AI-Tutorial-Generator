4bee9c3d1801cedf918624bc6951b577a29340d7
Asis <asiso7@gmail.com>
2022-03-17 10:22:57 +0100

    added try/except in loop as fix

--- get-youtube-scripts.py
+++ get-youtube-scripts.py
@@ -1,17 +1,23 @@
 # %%
-# import urllib.parse as p
 import re
 import os
 import pickle
+import pandas as pd
 
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+
 from youtube_transcript_api import YouTubeTranscriptApi
 
-QUERY = input()
+# --------------
+# VARIABLES
+# --------------
+
+QUERY = input("Enter YouTube search query: ")
 SCOPES = ["https://www.googleapis.com/auth/youtube.force-ssl"]
-SECRET = "/Users/alcachofa/Documents/Encrpyted/secrets/credentials.json"
+SECRET = "credentials.json" # update with your credentials file here
+max_results = 100
 
 # --------------
 # FUNCTIONS
@@ -28,7 +34,7 @@ def youtube_authenticate():
     if os.path.exists("token.pickle"):
         with open("token.pickle", "rb") as token:
             creds = pickle.load(token)
-    # if there are no (valid) credentials availablle, let the user log in.
+    # if there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -47,7 +53,6 @@ def search(youtube, **kwargs):
         part="snippet",
         **kwargs
     ).execute()
-    
 
 # def get_video_details(youtube, **kwargs):
 #     return youtube.videos().list(
@@ -55,17 +60,23 @@ def search(youtube, **kwargs):
 #         **kwargs
 #     ).execute()
 
-# %%
+#%%
 # --------------
 # MAIN PART
 # --------------
+
+# print results parameter
+print(f"--- Note: maximum results set to {max_results} ---")
 
 # authenticate to YouTube API
 youtube = youtube_authenticate()
 
 # perform search
-response = search(youtube, q=QUERY, maxResults=3)
+response = search(youtube, q=QUERY, maxResults=max_results)
+next_page = response['nextPageToken']
 items = response.get("items")
+
+# TODO: Add another parent loop to iterate over next_page token
 
 # extract elements
 video_list = []
@@ -81,17 +92,37 @@ for i in items:
                   'publish_date': publish_date
                   }
     video_list.append(video_data) # append to list
-    print(f"fetching... {title}")
-# %%
+    print(f"Fetching video: {title}")
+
+
 
 # Iterate over video-id's and create .txt files
-video_script = ''
+videos_with_subtitles = []
 for i in video_list:
+    video_script = '' # initialise empty string
     id = i['id']
-    print(f"saving .txt file... {id}")
-    video_transcript = YouTubeTranscriptApi.get_transcript(id, languages=['en'])
-    for i in video_transcript:
-        text = i['text']+' '
-        video_script = video_script + text 
-    with open(f"./scripts/{id}.txt", "w") as text_file:
-        print(video_script, file=text_file)
+    try:
+        video_transcript = YouTubeTranscriptApi.get_transcript(video_id=id, languages=['en'])
+        print(f"Saving subtitle file as {id}.txt")
+        videos_with_subtitles.append(id)
+        for j in video_transcript:
+            text = j['text'] + ' '
+            video_script = video_script + text 
+        with open(f"./scripts/{id}.txt", "w") as text_file:
+            print(video_script, file=text_file)
+    except:
+        print("-- Error: No subtitles available --")
+        pass
+
+
+# create df and filter by videos with subtitles available
+video_df = pd.DataFrame(video_list)
+video_df = video_df.loc[ video_df['id'].isin(videos_with_subtitles), : ]
+video_df['publish_date'] = pd.to_datetime(video_df['publish_date']).dt.date
+
+# export video meta-data as .csv
+video_df.to_csv('video_data.csv')
+print(f".csv file exported")
+print(f"{video_df.shape[0]} total files created")
+
+# %%
